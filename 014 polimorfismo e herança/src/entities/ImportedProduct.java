b9ad1450e26a9f5fd1a92266a111962727b15016
GabrielMarchiori <gabriel.marchiori202@gmail.com>
2022-09-16 12:51:02 -0300

    criada a estrutura padrão

--- 014 polimorfismo e herança/src/entities/ImportedProduct.java
+++ 014 polimorfismo e herança/src/entities/ImportedProduct.java
@@ -1,5 +1,32 @@
 package entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product {
     
+    private Double customsFee;
+
+    public ImportedProduct() {
+        super();
+    }
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    public Double totalPrice(){
+        return getPrice()+getCustomsFee();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " (Customs fee: $ " + getCustomsFee();
+    }    
 }
--- 014 polimorfismo e herança/src/entities/UsedProduct.java
+++ 014 polimorfismo e herança/src/entities/UsedProduct.java
@@ -1,5 +1,30 @@
 package entities;
 
-public class UsedProduct {
+import java.time.LocalDate;
+
+public class UsedProduct extends Product {
+
+    private LocalDate date;
+
+    public UsedProduct() {
+        super();
+    }
+
+    public UsedProduct(String name, Double price, LocalDate date) {
+        super(name, price);
+        this.date = date;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    
+
+    
     
 }
